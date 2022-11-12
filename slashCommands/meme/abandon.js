2af9f303bc5fef2a5203ba6b0c4e083a66b02f19
XCALIBUR <sabitahmad555@gmail.com>
2022-11-12 15:01:02 +0600

    added abandon.js

---
+++ slashCommands/meme/abandon.js
@@ -0,0 +1,40 @@
+const { ApplicationCommandType, ApplicationCommandOptionType, AttachmentBuilder, EmbedBuilder} = require('discord.js');
+
+module.exports = {
+    name: 'abandon',
+    description: "Generate abandon meme",
+    type: ApplicationCommandType.ChatInput,
+    cooldown: 3000,
+    options: [
+        {
+            name: 'text',
+            description: 'Input note text',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+
+
+
+    ],
+    run: async (client, interaction) => {
+        await interaction.deferReply()
+
+        client.memer.abandon(interaction.options.get('text').value).then(async image => {
+
+            const attachment = new AttachmentBuilder(image, {name: 'abandon.png'})
+
+            const embed = new EmbedBuilder()
+                .setTitle(`Note for ${interaction.user.tag}`)
+                .setColor('Blue')
+                .setImage('attachment://abandon.png')
+                .setTimestamp()
+
+            await interaction.editReply({embeds: [embed], files: [attachment]})
+
+        }).catch(async err => {
+            await interaction.editReply('Error' + err)
+
+        })
+
+    }
+};
